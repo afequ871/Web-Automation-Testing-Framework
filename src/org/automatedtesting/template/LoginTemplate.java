1169d3aa12eed7973afd915e262cbadd0ab24665
Andal FeQuiere <andal.fequiere@us.pwc.com>
2013-09-23 17:04:42 -0400

    updated correct path

---
+++ src/org/automatedtesting/template/LoginTemplate.java
@@ -0,0 +1,68 @@
+package org.automatedtesting.template;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class LoginTemplate {	
+	
+	private String loginHeaderLbl;	
+	private String loginUserName;	
+	private String loginPassword;	
+	
+	
+	public LoginTemplate(Properties propFile){
+		loginHeaderLbl = propFile.getProperty("loginHeaderLbl");
+		loginUserName = propFile.getProperty("loginUserName");
+		loginPassword = propFile.getProperty("loginPassword");
+		
+		
+		
+	}
+	
+	public String getLoginHeaderLbl() {
+		return loginHeaderLbl;
+	}
+
+	public void setLoginHeaderLbl(String loginHeaderLbl) {
+		this.loginHeaderLbl = loginHeaderLbl;
+	}
+
+	public String getLoginUserName() {
+		return loginUserName;
+	}
+
+	public void setLoginUserName(String loginUserName) {
+		this.loginUserName = loginUserName;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+	
+	public Properties getProperties() {
+		Properties prop = new Properties();
+		InputStream in = getClass().getResourceAsStream("suite.properties");
+		try {
+			prop.load(in);
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		return prop;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
